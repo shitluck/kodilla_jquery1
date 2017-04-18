1de790c7b3695a4ebc4a08a9303bbe3b17b4e878
Mac Ryndziewicz <fuji.two6@gmail.com>
2017-04-18 12:04:48 +0200

    changed code in html and js files

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
-﻿$(document).ready(function(){
+﻿var span = $('span');
+var paragraphs = $('p');
+
+$(document).ready(function(){
 	console.log('DOM załadowany - można się bawić');
 });
-
-var span = $('span');
 
 span.each(function (index, element){
     if(index % 2 === 0) {
         $(element).css('color', 'red');
     };
 });
-
-var paragraphs = $('p');
 
 paragraphs.each(function (index, element){
     var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
